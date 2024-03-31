cf19f43f7549923fcc12072b6867654a908e90c8
Victor Sandru <vsandru4@gmail.com>
2024-03-30 20:37:07 -0400

    added ddl

---
+++ db_schema_creation.sql
@@ -0,0 +1,26 @@
+-- CREATE DATABASE GymDB;
+
+-- CREATE TABLE Admins (
+--     admin_id SERIAL PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     phone_number INTEGER NOT NULL
+-- );
+
+-- CREATE TABLE Members (
+--     member_id SERIAL PRIMARY KEY,
+--     member_email VARCHAR(255) NOT NULL,
+--     member_name VARCHAR(255) NOT NULL,
+--     member_
+-- );
+
+-- CREATE TABLE Equipment (
+--     equipment_id SERIAL PRIMARY KEY,
+--     equipment_name VARCHAR(255) NOT NULL,
+--     maintenance_status VARCHAR(255)
+-- );
+
+-- CREATE TABLE Billing (
+--     billing_id SERIAL PRIMARY KEY,
+
+-- )
